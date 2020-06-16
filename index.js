a0679a9521720ff01b903e993da2f8796a97d1a3
popdf10 <popdf10@gmail.com>
2020-06-16 11:26:03 +0900

    run server

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+function handleHome(req, res) {
+  console.log(req);
+  res.send("Hello from Home!");
+}
+
+app.get("/", handleHome);
+app.get("/profile", function (req, res) {
+  res.send("You are on my profile");
+});
+
+app.listen(PORT, handleListening);

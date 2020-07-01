54ed13796fcb351a68d25900755d97d7b87b3509
popdf10 <popdf10@gmail.com>
2020-07-01 17:53:29 +0900

    #3.6

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -5,13 +5,14 @@ import {
   postUpload,
   videoDetail,
   editVideo,
-  deleteVideo
+  deleteVideo,
 } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);

ad907fcbef6fc06076282d1904ef254c8d87f502
popdf10 <popdf10@gmail.com>
2020-07-08 09:19:59 +0900

    #6.4

--- init.js
+++ init.js
@@ -3,6 +3,8 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
+import "./models/User";
 
 const PORT = process.env.PORT || 4000;
 
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -6,16 +6,17 @@ import {
   getLogin,
   logout,
   postJoin,
-  postLogin
+  postLogin,
 } from "../controllers/userController";
+import { onlyPublic } from "../middlewares";
 
 const globalRouter = express.Router();
 
-globalRouter.get(routes.join, getJoin);
-globalRouter.post(routes.join, postJoin);
+globalRouter.get(routes.join, onlyPublic, getJoin);
+globalRouter.post(routes.join, onlyPublic, postJoin, postLogin);
 
-globalRouter.get(routes.login, getLogin);
-globalRouter.post(routes.login, postLogin);
+globalRouter.get(routes.login, onlyPublic, getLogin);
+globalRouter.post(routes.login, onlyPublic, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
